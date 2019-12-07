c8af9f7b47a990bcfaa39f9be52494d8f572d0ca
Ratakondala Arun <arunratakondala3470@gmail.com>
2019-12-07 17:09:51 +0530

    added error code and handlers

---
+++ lib/constants/error_codes.dart
@@ -0,0 +1,16 @@
+class ValidationErrors {
+  // static const String ERR200 = '200';
+  //id error
+  static const String ERR404_1 = '404.1\"\"381';
+
+  //modeerror
+  static const String ERR404_2 = '404.2';
+  //everything is good
+  static const String ERR380 = '380';
+  //already updated
+  static const String ERR381 = '381';
+  //midc error
+  static const String ERR382 = '382';
+  //data updated successfully
+  static const String ERR200 = '200';
+}
--- lib/pages/qr_reader.dart
+++ lib/pages/qr_reader.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:barcode_scan/barcode_scan.dart';
@@ -5,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sqldemo/constants/constants.dart';
 import 'package:http/http.dart' as http;
+import 'package:sqldemo/constants/error_codes.dart';
+import 'package:toast/toast.dart';
 
 class QR extends StatelessWidget {
   @override
@@ -25,13 +28,15 @@ class _ReaderState extends State<Reader> {
     //render page
     return Scaffold(
       body: Container(
-        child: Center(
-            child: Column(
-          children: <Widget>[
-            Text(_scanResultId),
-            _modes(),
-          ],
-        )),
+        child: SingleChildScrollView(
+          child: Center(
+              child: Column(
+            children: <Widget>[
+              Text(_scanResultId),
+              _modes(),
+            ],
+          )),
+        ),
       ),
     );
   }
@@ -89,21 +94,42 @@ class _ReaderState extends State<Reader> {
     try {
       var url = DataBaseConstants.DATABASE_PROCESS_URL;
       http.Response response = await http.post(url, body: data);
-      print('response code = ${response.statusCode}');
+      print('site response code = ${response.statusCode}');
       if (response.statusCode == 200) {
         print(response.body);
-        return response.body;
+        // print(jsonDecode(response.body[0]));
+        Toast.show('${_validatingUserId(response.body.toString())}', context,
+            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+        return;
+      } else if (response.statusCode == 500) {
+        Toast.show('There is an error with server!', context,
+            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+        return;
       } else {
+        Toast.show('There is an unknown error occurred', context,
+            duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
         print('unable to send data! responsecode = ${response.statusCode}');
       }
     } on FormatException {
       //todo:user dialag
-      print('formatexception in qr_reader.dart in _sendingdata function');
+      print('formatexception in qr_reader.dart in _senddata function');
     } on SocketException {
       print('Socket Exception');
+      Toast.show('Check your internet connection', context,
+          duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
       //socketException may occur if user changes
       // from wifi to mobile data while http request is ongoing
     }
+  }
+
+  String _validatingUserId(String code) {
+    print(code);
+    if (code.contains(ValidationErrors.ERR200)) return 'Updated successfully';
+    if (code.contains(ValidationErrors.ERR380)) return 'Already collected';
+    if (code.contains(ValidationErrors.ERR404_1)) return 'Invalid user';
+    if (code.contains(ValidationErrors.ERR381)) return 'Already taken the food';
+    if (code.contains(ValidationErrors.ERR382)) return 'Miscellaneous error';
+    return 'unknown error occurred';
   }
 
   //----------------------------------------UI-REUSABLES-----------------------------
