09de64a68b63dffa06a70cbdee0aab1c5a6442ce
Ratakondala Arun <arunratakondala3470@gmail.com>
2019-12-08 16:10:55 +0530

    removed -details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sqldemo/pages/details.dart';
 import 'package:sqldemo/pages/qr_reader.dart';
 
 void main() => runApp(MyApp());
@@ -17,67 +16,5 @@ class MyApp extends StatelessWidget {
       ),
       home: QR(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // TabController _tabController;
-
-  @override
-  void initState() {
-    // _tabController = TabController(length: 3);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('GDS'),
-          bottom: TabBar(
-            isScrollable: false,
-            // controller: _tabController,
-            tabs: _tabs(),
-          ),
-        ),
-        body: TabBarView(
-          children: _body(),
-        ),
-      ),
-    );
-  }
-
-  List<Widget> _tabs() {
-    return <Widget>[
-      Tab(
-        icon: Icon(Icons.restaurant_menu),
-      ),
-      Tab(
-        icon: Icon(Icons.camera_alt),
-      ),
-    ];
-  }
-
-  List<Widget> _body() {
-    return <Widget>[
-      Details(),
-      QR(),
-    ];
   }
 }
